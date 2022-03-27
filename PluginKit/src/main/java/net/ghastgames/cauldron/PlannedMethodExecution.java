49bc405649da3db8c800aee8c3c62e7a5e62a7a8
Jonah <56933799+jonah07@users.noreply.github.com>
2022-03-27 15:35:44 +0200

    Update PlannedMethodExecution.java

--- PluginKit/src/main/java/net/ghastgames/cauldron/PlannedMethodExecution.java
+++ PluginKit/src/main/java/net/ghastgames/cauldron/PlannedMethodExecution.java
@@ -1,10 +1,12 @@
 package net.ghastgames.cauldron;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.lang.reflect.Method;
 
 @Data
+@AllArgsConstructor
 public class PlannedMethodExecution {
     private PluginKit.HookEvent event;
     private Method method;
