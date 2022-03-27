2ee2aa1e656c4df2850d67c22285423191b363f4
Jonah <56933799+jonah07@users.noreply.github.com>
2022-03-27 18:55:05 +0200

    Added ``registerCommand``

---
+++ PluginKit/src/main/java/net/ghastgames/cauldron/pluginkit/PluginKit.java
@@ -0,0 +1,45 @@
+package net.ghastgames.cauldron.pluginkit;
+
+import net.ghastgames.cauldron.pluginkit.annotations.CommandDetails;
+import net.ghastgames.cauldron.pluginkit.commands.CauldronCommand;
+import net.ghastgames.cauldron.pluginkit.commands.CommandExecution;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
+import java.util.Random;
+
+public class PluginKit {
+    private static JavaPlugin plugin;
+
+    public static void init(JavaPlugin javaPlugin) {
+        plugin = javaPlugin;
+    }
+
+    public static void registerCommand(CauldronCommand command) {
+        String commandName = "";
+        String[] permissions = {};
+
+        if(command.getClass().isAnnotationPresent(CommandDetails.class)) {
+            CommandDetails details = command.getClass().getAnnotation(CommandDetails.class);
+            commandName = Objects.requireNonNullElse(details.name(), "");
+            permissions = Objects.requireNonNullElse(details.permissions(), new String[]{""});
+        } else {
+            commandName = "";
+            permissions = new String[]{""};
+        }
+
+        if(commandName.isBlank()) {
+            commandName = "ghastgames-" + new Random().nextInt();
+        }
+
+        plugin.getCommand(commandName).setExecutor(new CommandExecutor() {
+            @Override
+            public boolean onCommand(CommandSender commandSender, Command c, String s, String[] strings) {
+                return command.execute(new CommandExecution(commandSender, strings));
+            }
+        });
+    }
+}
