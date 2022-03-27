f379091d4f60747af7af638d8b3aeb13a17e4d48
Jonah <56933799+jonah07@users.noreply.github.com>
2022-03-27 15:35:40 +0200

    Update PluginKit.java

--- PluginKit/src/main/java/net/ghastgames/cauldron/PluginKit.java
+++ PluginKit/src/main/java/net/ghastgames/cauldron/PluginKit.java
@@ -1,11 +1,14 @@
 package net.ghastgames.cauldron;
 
+import net.ghastgames.cauldron.annotations.DisableHook;
+
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.List;
 
 public class PluginKit {
     private static MinecraftPlugin minecraftPlugin;
-    private List<PlannedMethodExecution> hooks;
+    private static List<PlannedMethodExecution> hooks;
 
     enum HookEvent {
         ENABLE, DISABLE
@@ -14,6 +17,11 @@ public class PluginKit {
     public static void initPlugin(Class<? extends MinecraftPlugin> plugin) {
         try {
             minecraftPlugin = plugin.newInstance();
+            for(Method method : plugin.getMethods()) {
+                if(method.isAnnotationPresent(DisableHook.class)) {
+                    hooks.add(new PlannedMethodExecution(HookEvent.DISABLE, method));
+                }
+            }
         } catch (InstantiationException | IllegalAccessException e) {
             e.printStackTrace();
         }
