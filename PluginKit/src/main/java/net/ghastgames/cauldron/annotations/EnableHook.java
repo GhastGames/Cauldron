9a02198f977d0e2b9c2ff69e209607507e7d467f
Jonah <56933799+jonah07@users.noreply.github.com>
2022-03-27 15:43:52 +0200

    Added retention policy

--- PluginKit/src/main/java/net/ghastgames/cauldron/annotations/EnableHook.java
+++ PluginKit/src/main/java/net/ghastgames/cauldron/annotations/EnableHook.java
@@ -1,4 +1,8 @@
 package net.ghastgames.cauldron.annotations;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface EnableHook {
 }
