fddee788b8d30462704bc63a21967945bc35fad3
Jonah <56933799+jonah07@users.noreply.github.com>
2022-03-28 20:04:22 +0200

    Update VisualKit.java

--- VisualKit/src/main/java/net/ghastgames/cauldron/visualkit/VisualKit.java
+++ VisualKit/src/main/java/net/ghastgames/cauldron/visualkit/VisualKit.java
@@ -3,12 +3,12 @@ package net.ghastgames.cauldron.visualkit;
 import net.ghastgames.cauldron.visualkit.scoreboards.ScoreboardManager;
 
 public class VisualKit {
-    private VisualKit instance;
-    private ScoreboardManager scoreboardManager;
+    private static VisualKit instance;
+    private static ScoreboardManager scoreboardManager;
 
     private VisualKit() {}
 
-    public VisualKit getInstance() {
+    public static VisualKit getInstance() {
         if(instance == null) {
             instance = new VisualKit();
         }
