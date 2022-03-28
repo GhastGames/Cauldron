593daf81aea0308505a8f6e1aea6b0e5cb540d5e
Jonah <56933799+jonah07@users.noreply.github.com>
2022-03-28 20:04:17 +0200

    Update ScoreboardManager.java

--- VisualKit/src/main/java/net/ghastgames/cauldron/visualkit/scoreboards/ScoreboardManager.java
+++ VisualKit/src/main/java/net/ghastgames/cauldron/visualkit/scoreboards/ScoreboardManager.java
@@ -1,4 +1,17 @@
 package net.ghastgames.cauldron.visualkit.scoreboards;
 
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+
 public class ScoreboardManager {
+    private HashMap<Player, Scoreboard> scoreboards;
+    private static ScoreboardManager instance;
+
+    private ScoreboardManager() {}
+
+    public static ScoreboardManager getInstance() {
+        if(instance == null) instance = new ScoreboardManager();
+        return instance;
+    }
 }
