312588e48171d1bda34232071ce397a15e4e327f
Jonah <56933799+jonah07@users.noreply.github.com>
2022-03-29 21:07:36 +0200

    Fix bug

--- VisualKit/src/main/java/net/ghastgames/cauldron/visualkit/AnimationUtils.java
+++ VisualKit/src/main/java/net/ghastgames/cauldron/visualkit/AnimationUtils.java
@@ -3,6 +3,7 @@ package net.ghastgames.cauldron.visualkit;
 public class AnimationUtils {
 
     public static String markCharacter(String markWith, String continueWith, String text, int position) {
+        position = position - 1;
         return text.substring(0, position) + markWith + text.charAt(position) + continueWith + text.substring(position + 1);
     }
 }
