b2fa47169a702574951f8a1e440d5cc385c4c9c8
Jonah <56933799+jonah07@users.noreply.github.com>
2022-03-29 19:46:08 +0200

    Created interface for all managers

---
+++ VisualKit/src/main/java/net/ghastgames/cauldron/visualkit/VisualManager.java
@@ -0,0 +1,8 @@
+package net.ghastgames.cauldron.visualkit;
+
+import org.bukkit.entity.Player;
+
+public interface VisualManager<T, U> {
+    void set(Player player, T object);
+    void update(Player player, U newObject);
+}
