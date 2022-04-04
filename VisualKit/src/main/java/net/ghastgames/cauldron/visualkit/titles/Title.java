133a4dacb9e8272b64aa04fb17494db4b6a94e92
Jonah <56933799+jonah07@users.noreply.github.com>
2022-04-04 15:37:35 +0200

    Now working on titles

---
+++ VisualKit/src/main/java/net/ghastgames/cauldron/visualkit/titles/Title.java
@@ -0,0 +1,4 @@
+package net.ghastgames.cauldron.visualkit.titles;
+
+public class Title {
+}
---
+++ VisualKit/src/main/java/net/ghastgames/cauldron/visualkit/titles/TitleManager.java
@@ -0,0 +1,21 @@
+package net.ghastgames.cauldron.visualkit.titles;
+
+import net.ghastgames.cauldron.visualkit.VisualManager;
+import org.bukkit.entity.Player;
+
+public class TitleManager implements VisualManager<String, String> {
+    @Override
+    public void set(Player player, String object) {
+
+    }
+
+    @Override
+    public void update(Player player, String newObject) {
+
+    }
+
+    @Override
+    public String get(Player player) {
+        return null;
+    }
+}
