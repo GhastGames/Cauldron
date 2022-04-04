0a44a8392b3dadda4cb030cc7f2d62bae7a16aaf
Jonah <56933799+jonah07@users.noreply.github.com>
2022-04-04 16:25:30 +0200

    Update Title.java

--- VisualKit/src/main/java/net/ghastgames/cauldron/visualkit/titles/Title.java
+++ VisualKit/src/main/java/net/ghastgames/cauldron/visualkit/titles/Title.java
@@ -1,4 +1,14 @@
 package net.ghastgames.cauldron.visualkit.titles;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Title {
+    String title;
+    String subtitle;
+    int fadeIn;
+    int fadeOut;
+    int stay;
 }
