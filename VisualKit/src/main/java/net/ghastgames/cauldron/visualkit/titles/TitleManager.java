ddb23133ee9fcef6b1ac5b4e3d4a0e6af7f581ce
Jonah <56933799+jonah07@users.noreply.github.com>
2022-04-04 16:25:36 +0200

    Finished TitleManager

--- VisualKit/src/main/java/net/ghastgames/cauldron/visualkit/titles/TitleManager.java
+++ VisualKit/src/main/java/net/ghastgames/cauldron/visualkit/titles/TitleManager.java
@@ -1,21 +1,34 @@
 package net.ghastgames.cauldron.visualkit.titles;
 
 import net.ghastgames.cauldron.visualkit.VisualManager;
+import net.minecraft.server.v1_8_R3.IChatBaseComponent;
+import net.minecraft.server.v1_8_R3.PacketPlayOutTitle;
+import org.apache.commons.lang.StringEscapeUtils;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
-public class TitleManager implements VisualManager<String, String> {
-    @Override
-    public void set(Player player, String object) {
+public class TitleManager implements VisualManager<Title, Title> {
 
+    @Override
+    public void set(Player player, Title newTitle) {
+        CraftPlayer craftPlayer = (CraftPlayer) player;
+        IChatBaseComponent title = IChatBaseComponent.ChatSerializer.a("{text: \"" + newTitle.getTitle().replace("\"", "") + "\"}");
+        PacketPlayOutTitle packetPlayOutTitle = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TITLE, title, newTitle.fadeIn, newTitle.stay, newTitle.fadeOut);
+
+        IChatBaseComponent subtitle = IChatBaseComponent.ChatSerializer.a("{text: \"" + newTitle.getTitle().replace("\"", "") + "\"}");
+        PacketPlayOutTitle packetPlayOutTitle2 = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.SUBTITLE, subtitle, newTitle.fadeIn, newTitle.stay, newTitle.fadeOut);
+
+        craftPlayer.getHandle().playerConnection.sendPacket(packetPlayOutTitle);
+        craftPlayer.getHandle().playerConnection.sendPacket(packetPlayOutTitle2);
     }
 
     @Override
-    public void update(Player player, String newObject) {
-
+    public void update(Player player, Title newObject) {
+        set(player, newObject);
     }
 
     @Override
-    public String get(Player player) {
+    public Title get(Player player) {
         return null;
     }
 }
