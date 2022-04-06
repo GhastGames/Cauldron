7ad816ae63134d9a7c480537103ffff9bbefd66a
Jonah <56933799+jonah07@users.noreply.github.com>
2022-04-06 21:59:08 +0200

    Working on StorageKit

--- StorageKit/src/main/java/net/ghastgames/cauldron/storagekit/StorageKit.java
+++ StorageKit/src/main/java/net/ghastgames/cauldron/storagekit/StorageKit.java
@@ -1,12 +1,18 @@
 package net.ghastgames.cauldron.storagekit;
 
+import net.ghastgames.cauldron.pluginkit.PluginKit;
+
 public class StorageKit {
     private static StorageKit instance;
+
+    static {
+        PluginKit.getPlugin().saveConfig();
+    }
 
     public static StorageKit getInstance() {
         if(instance == null) instance = new StorageKit();
         return instance;
     }
 
-    
+    public void
 }
