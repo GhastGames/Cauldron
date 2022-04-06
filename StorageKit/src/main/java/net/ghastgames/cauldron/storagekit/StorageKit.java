5da8f051d4345e2ac60f57d111a422e68f9cbbe5
Jonah <56933799+jonah07@users.noreply.github.com>
2022-04-06 20:42:21 +0200

    getInstance method

--- StorageKit/src/main/java/net/ghastgames/cauldron/storagekit/StorageKit.java
+++ StorageKit/src/main/java/net/ghastgames/cauldron/storagekit/StorageKit.java
@@ -1,4 +1,12 @@
 package net.ghastgames.cauldron.storagekit;
 
 public class StorageKit {
+    private static StorageKit instance;
+
+    public static StorageKit getInstance() {
+        if(instance == null) instance = new StorageKit();
+        return instance;
+    }
+
+    
 }
