8a092b6b7308c0923362461d65dee0b932a78957
Jonah <56933799+jonah07@users.noreply.github.com>
2022-04-07 21:37:25 +0200

    Create ProxyInstance.java

---
+++ CloudKit/src/main/java/net/ghastgames/cauldron/instances/ProxyInstance.java
@@ -0,0 +1,17 @@
+package net.ghastgames.cauldron.instances;
+
+import net.ghastgames.cauldron.CauldronInstance;
+import net.ghastgames.cauldron.ServerStartup;
+
+public class ProxyInstance extends CauldronInstance {
+
+    @Override
+    public void start(ServerStartup info) {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
