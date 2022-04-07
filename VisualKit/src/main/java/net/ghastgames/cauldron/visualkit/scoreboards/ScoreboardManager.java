1296feadc8c0357156df74274123cba3264f1595
Jonah <56933799+jonah07@users.noreply.github.com>
2022-04-07 15:21:28 +0200

    Fix another NPE

--- VisualKit/src/main/java/net/ghastgames/cauldron/visualkit/scoreboards/ScoreboardManager.java
+++ VisualKit/src/main/java/net/ghastgames/cauldron/visualkit/scoreboards/ScoreboardManager.java
@@ -39,7 +39,7 @@ public class ScoreboardManager implements VisualManager<CauldronScoreboard, Scor
                 Team dynamicValue = scoreboard.registerNewTeam(value.id);
                 dynamicValue.addEntry(placeholders[count]);
                 dynamicValue.setPrefix(value.get());
-                if(value.get().length() >= 14) {
+                if(value.get().length() >= 16) {
                     dynamicValue.setPrefix(value.get().substring(0, 16));
                     try {
                         dynamicValue.setSuffix(value.get().substring(16).isEmpty() ? "" : value.get().substring(16));
