ceb1af992a96ce9b5efe0e13ef1e87c5b60d7782
Jonah <56933799+jonah07@users.noreply.github.com>
2022-04-08 12:01:04 +0200

    Deleted CloudKit files, rewrite

--- CloudKit/src/main/java/net/ghastgames/cauldron/CauldronInstance.java
+++
@@ -1,6 +0,0 @@
-package net.ghastgames.cauldron;
-
-public abstract class CauldronInstance {
-    public abstract void start(ServerStartup startupDetails);
-    public abstract void stop();
-}
--- CloudKit/src/main/java/net/ghastgames/cauldron/ServerStartup.java
+++
@@ -1,9 +0,0 @@
-package net.ghastgames.cauldron;
-
-import lombok.Data;
-
-@Data
-public class ServerStartup {
-    int minRam;
-    int maxRam;
-}
