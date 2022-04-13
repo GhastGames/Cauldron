bf5e2f03b5f5ed164b3812d3fce6d217b5cedc1f
Jonah <56933799+jonah07@users.noreply.github.com>
2022-04-13 07:17:40 +0200

    Config

--- cloudkit-remote/src/main/java/net/ghastgames/cauldron/cloudkit/Remote.java
+++ cloudkit-remote/src/main/java/net/ghastgames/cauldron/cloudkit/Remote.java
@@ -1,8 +1,27 @@
 package net.ghastgames.cauldron.cloudkit;
 
+import com.google.gson.Gson;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Remote {
 
-    public static void main(String[] args) {
+    private static final Gson gson = new Gson();
+
+    public static void main(String[] args) throws IOException {
+        String relativeConfigPath = "./cloudkit.remote.config.json";
+
+        if(!Paths.get(relativeConfigPath).toFile().exists()) {
+            CKRemoteConfig defaultConfig = new CKRemoteConfig("localhost", "0", "your-password");
+            BufferedWriter writer = new BufferedWriter(new FileWriter(relativeConfigPath));
+            writer.write(gson.toJson(defaultConfig));
+            writer.close();
+        }
+        String config = new String(Files.readAllBytes(Paths.get(relativeConfigPath)));
 
     }
 }
