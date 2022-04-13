c43ce477c00ebffdbdfd87eca212f076d2a0d9c1
Jonah <56933799+jonah07@users.noreply.github.com>
2022-04-13 11:32:21 +0200

    Create ConfigurationException.java

---
+++ cloudkit-remote/src/main/java/net/ghastgames/cauldron/cloudkit/exceptions/ConfigurationException.java
@@ -0,0 +1,8 @@
+package net.ghastgames.cauldron.cloudkit.exceptions;
+
+public class ConfigurationException extends Throwable {
+
+    public ConfigurationException(String message) {
+        super(message);
+    }
+}
