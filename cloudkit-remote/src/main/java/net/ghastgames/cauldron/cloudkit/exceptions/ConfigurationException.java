f1563b755b892d095053f23b955199e56204215d
Jonah <56933799+jonah07@users.noreply.github.com>
2022-04-15 13:47:29 +0200

    Deleted cloudkit-remote

--- cloudkit-remote/src/main/java/net/ghastgames/cauldron/cloudkit/exceptions/ConfigurationException.java
+++
@@ -1,8 +0,0 @@
-package net.ghastgames.cauldron.cloudkit.exceptions;
-
-public class ConfigurationException extends Throwable {
-
-    public ConfigurationException(String message) {
-        super(message);
-    }
-}
