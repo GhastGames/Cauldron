a90b81c3db6f2e6cceb484086219ace851d45849
Jonah <56933799+jonah07@users.noreply.github.com>
2022-04-16 08:35:41 +0200

    Sending Action Bars

--- VisualKit/src/main/java/net/ghastgames/cauldron/visualkit/actionbar/ActionbarManager.java
+++ VisualKit/src/main/java/net/ghastgames/cauldron/visualkit/actionbar/ActionbarManager.java
@@ -1,13 +1,17 @@
 package net.ghastgames.cauldron.visualkit.actionbar;
 
 import net.ghastgames.cauldron.visualkit.VisualManager;
+import net.minecraft.server.v1_8_R3.ChatBaseComponent;
+import net.minecraft.server.v1_8_R3.IChatBaseComponent;
+import net.minecraft.server.v1_8_R3.PacketPlayOutChat;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 public class ActionbarManager implements VisualManager<Actionbar, Actionbar> {
 
     @Override
     public void set(Player player, Actionbar object) {
-
+        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(new PacketPlayOutChat(IChatBaseComponent.ChatSerializer.a(object.getText()), (byte) 2));
     }
 
     @Override
