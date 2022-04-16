724429363f09435f823d9bb97e00cd56e88d589a
Jonah <56933799+jonah07@users.noreply.github.com>
2022-04-16 08:23:48 +0200

    Create ActionbarManager.java

---
+++ VisualKit/src/main/java/net/ghastgames/cauldron/visualkit/actionbar/ActionbarManager.java
@@ -0,0 +1,22 @@
+package net.ghastgames.cauldron.visualkit.actionbar;
+
+import net.ghastgames.cauldron.visualkit.VisualManager;
+import org.bukkit.entity.Player;
+
+public class ActionbarManager implements VisualManager<Actionbar, Actionbar> {
+
+    @Override
+    public void set(Player player, Actionbar object) {
+
+    }
+
+    @Override
+    public void update(Player player, Actionbar newObject) {
+
+    }
+
+    @Override
+    public Actionbar get(Player player) {
+        return null;
+    }
+}
