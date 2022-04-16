955899c4d3deeb4627af6b20c7d16c1786a0c548
Jonah <56933799+jonah07@users.noreply.github.com>
2022-04-16 08:32:03 +0200

    Update Actionbar.java

--- VisualKit/src/main/java/net/ghastgames/cauldron/visualkit/actionbar/Actionbar.java
+++ VisualKit/src/main/java/net/ghastgames/cauldron/visualkit/actionbar/Actionbar.java
@@ -1,8 +1,10 @@
 package net.ghastgames.cauldron.visualkit.actionbar;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Actionbar {
     String text;
 }
