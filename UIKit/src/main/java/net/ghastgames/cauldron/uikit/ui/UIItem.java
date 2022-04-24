8875e5c2d14d9824c9af4e0b1cbaa10ac7938dfc
Jonah <56933799+jonah07@users.noreply.github.com>
2022-04-24 18:27:19 +0200

    Create UIItem.java

---
+++ UIKit/src/main/java/net/ghastgames/cauldron/uikit/ui/UIItem.java
@@ -0,0 +1,34 @@
+package net.ghastgames.cauldron.uikit.ui;
+
+import lombok.Builder;
+import lombok.Data;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.List;
+
+@Data
+@Builder
+public class UIItem {
+    String name;
+    List<String> lore;
+    Material material;
+    Enchantment enchantment;
+    short subId;
+    int amount;
+    boolean showEnchantments;
+
+    ItemStack getItemStack() {
+        ItemStack itemStack = new ItemStack(material, amount);
+        ItemMeta meta = itemStack.getItemMeta();
+        meta.setLore(lore);
+        meta.setDisplayName(name);
+        if(!showEnchantments) meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        itemStack.setItemMeta(meta);
+        itemStack.addEnchantment(enchantment, 1);
+        return itemStack;
+    }
+}
