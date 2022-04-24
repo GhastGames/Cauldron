2ff2de8d72da7fa024778d21b26c0eef0b1b69f1
Jonah <56933799+jonah07@users.noreply.github.com>
2022-04-24 19:36:44 +0200

    Update UIKitInventoryHolder.java

--- UIKit/src/main/java/net/ghastgames/cauldron/uikit/ui/UIKitInventoryHolder.java
+++ UIKit/src/main/java/net/ghastgames/cauldron/uikit/ui/UIKitInventoryHolder.java
@@ -1,12 +1,14 @@
 package net.ghastgames.cauldron.uikit.ui;
 
+import lombok.Getter;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 
 import java.util.UUID;
 
 public class UIKitInventoryHolder implements InventoryHolder {
-    private UUID inventoryID;
+    @Getter
+    private final UUID inventoryID;
 
     public UIKitInventoryHolder(UUID inventoryID) {
         this.inventoryID = UUID.randomUUID();
