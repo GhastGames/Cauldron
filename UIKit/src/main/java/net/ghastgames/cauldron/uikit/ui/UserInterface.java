7500fa4efefd0cab6674b270d016d9466c38027a
Jonah <56933799+jonah07@users.noreply.github.com>
2022-04-24 19:45:10 +0200

    Removed unused import

--- UIKit/src/main/java/net/ghastgames/cauldron/uikit/ui/UserInterface.java
+++ UIKit/src/main/java/net/ghastgames/cauldron/uikit/ui/UserInterface.java
@@ -11,7 +11,6 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.UUID;
 
 public class UserInterface {
