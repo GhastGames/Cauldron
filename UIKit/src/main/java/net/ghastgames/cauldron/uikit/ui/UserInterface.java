61b4ff39831129430fae809e71970bdc18ab93f7
Jonah <56933799+jonah07@users.noreply.github.com>
2022-04-24 19:24:58 +0200

    Update UserInterface.java

--- UIKit/src/main/java/net/ghastgames/cauldron/uikit/ui/UserInterface.java
+++ UIKit/src/main/java/net/ghastgames/cauldron/uikit/ui/UserInterface.java
@@ -1,8 +1,14 @@
 package net.ghastgames.cauldron.uikit.ui;
 
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.Inventory;
+
+import java.util.UUID;
+
 public class UserInterface {
     String title;
     int slots;
+    Inventory inventory;
 
     public UserInterface(String title, int rows) {
         this.title = title;
@@ -10,7 +16,8 @@ public class UserInterface {
             throw new IllegalArgumentException("Too many rows");
         }
         this.slots = rows * 9; // rows * columns
+
+        inventory = Bukkit.createInventory(new UIKitInventoryHolder(UUID.randomUUID()), this.slots, title);
+
     }
-
-
 }
