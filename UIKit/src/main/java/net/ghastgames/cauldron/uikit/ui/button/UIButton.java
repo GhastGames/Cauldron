f6e2e462c658d872df8fef44294183d6016258bb
Jonah <56933799+jonah07@users.noreply.github.com>
2022-04-24 19:36:30 +0200

    Create UIButton.java

---
+++ UIKit/src/main/java/net/ghastgames/cauldron/uikit/ui/button/UIButton.java
@@ -0,0 +1,14 @@
+package net.ghastgames.cauldron.uikit.ui.button;
+
+import lombok.Data;
+import net.ghastgames.cauldron.uikit.ui.UIItem;
+import org.bukkit.entity.Player;
+
+@Data
+public class UIButton {
+    interface ClickAction {
+        void onClick(Player player);
+    }
+    UIItem item;
+    ClickAction action;
+}
